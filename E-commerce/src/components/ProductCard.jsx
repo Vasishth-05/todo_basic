c6e9cf9544c0a48b3a2528d0293ab5896dab2843
Vasishth <learnvasishth@gmail.com>
2026-05-14 09:23:57 +0530

    modified product card

--- E-commerce/src/components/ProductCard.jsx
+++ E-commerce/src/components/ProductCard.jsx
@@ -1,12 +1,13 @@
 function ProductCard({product}){
     return <div>
-        <div class='' >{`
+        <div class='flex pb-6 border-dashed ' >{`
             ID:${product.id},
             Product_title:${product.title},
             Product_price:${product.price},
             Product_category:${product.category}`
-        }</div>
+        }
         <button>Add to cart</button>
+        </div>
     </div>
 }
 
