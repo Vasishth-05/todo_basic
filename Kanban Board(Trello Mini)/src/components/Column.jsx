1192c94112b6557e5158951634db5ba2246714d0
Vasishth <learnvasishth@gmail.com>
2026-05-21 15:10:00 +0530

    add tailwind corrected

--- Kanban Board(Trello Mini)/src/components/Column.jsx
+++ Kanban Board(Trello Mini)/src/components/Column.jsx
@@ -1,7 +1,7 @@
 import TaskCard from "./TaskCard";
 
 function Column({title,tasks}){
-    return <div className="bg-gray-100 rounded-xl p-4 min-h-[500px]">
+    return <div className="bg-gray-100 rounded-xl p-4 min-h-125">
         <div className="font-bold text-lg mb-4">{title}</div>
         <div>
             {tasks.map((task) => (
